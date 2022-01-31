1dd2957d9a48e08534d64f6a8918f816969196db
NikResh <ka4mrve4erom@gmail.com>
2022-01-31 16:56:44 +0300

    Выполнено ДЗ первой недели

--- app/src/main/java/com/example/loftmoney/MainActivity.java
+++ app/src/main/java/com/example/loftmoney/MainActivity.java
@@ -1,23 +1,8 @@
 package com.example.loftmoney;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.example.loftmoney.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
